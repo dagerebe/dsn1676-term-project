2fa25be25b6d5d2e57a35b72335d889e026418f5
dagerebe <rebeccadagenais@Rebeccas-MacBook-Pro-3.local>
2015-03-18 03:03:14 -0400

    Added in wave and blizzard

--- js/main.js
+++ js/main.js
@@ -1,6 +1,26 @@
 var $BtnTornado = $('.tornado')
 var $tornado = $('.pictornado')
 
+var $BtnWave = $('.wave')
+var $wave = $('.picwave')
+
+var $BtnSnow = $('.snow')
+var $snow = $('.picsnow')
+
+var $houseBad = $('.house-bad')
+
+var $houseGood = $('.house-good')
+
 $BtnTornado.on('click', function () {
 	$tornado.toggleClass('js-tornado-in');
+	$houseBad.toggleClass('js-house-bad');
+	$houseGood.toggleClass('js-house-good');
+});
+
+$BtnWave.on('click', function () {
+	$wave.toggleClass('js-wave-in');
+});
+
+$BtnSnow.on('click', function () {
+	$snow.toggleClass('js-snow-in');
 });
